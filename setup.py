b71bed7316168af2b1f80f1668aab2df2bd7edfd
Krishna <krishnacool781@gmail.com>
2024-05-23 10:07:47 +0530

    version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mask_key',
-    version='0.1.0',
+    version='0.1.1',
     author='krishna agarwal',
     author_email='krishnacool781@gmail.com',
     description='A Python package to generate mask keys.',
